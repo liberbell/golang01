0435bed6764a7233d39e1361baed448eb4efbcfb
Liberbell <liberbell@gmail.com>
2019-06-09 11:10:32 +0900

    books_test3

--- books_test.go
+++ books_test.go
@@ -3,5 +3,9 @@ package api
 import "testing"
 
 func TestBookToJSON(t *testing.T) {
-	assert.True(t, true, "Impliment me.")
+	assert.True(t, true, "Implement me.")
+}
+
+func TestBookFromJSON(t *testing.T) {
+	assert.True(t, treu, "Implement me.")
 }
