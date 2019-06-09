b005c90dfb93ee312f9bcc57ddc60c9d5ff271e9
Liberbell <liberbell@gmail.com>
2019-06-10 07:48:30 +0900

    books1

---
+++ api/books.go
@@ -0,0 +1,28 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Book struct {
+	Title  string
+	Author string
+	ISBN   string
+}
+
+func (b Book) ToJSON() []byte {
+	ToJSON, err := json.Marshal(b)
+	if err != nil {
+		panic(err)
+	}
+	return nil
+}
+
+func FromJSON(data []byte) Book {
+	return Book{}
+}
+
+func BooksHandleFunc(w http.ResponseWriter, r *http.Request) {
+	test
+}
