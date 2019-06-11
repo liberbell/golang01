ed6b2f9c924383e4670ae9d93a03fe8ebb405262
Liberbell <liberbell@gmail.com>
2019-06-11 15:25:56 +0900

    numbers2

---
+++  numbers.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x float64
+	var y float64
+
+	x = 1
+	y = 2
+
+	fmt.Printf("x=%v, type of %T\n", x, x)
+	fmt.Printf("y=%v, type of %T\n", y, y)
+}
