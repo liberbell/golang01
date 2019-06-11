c662eb9bb93c6d82145ace29021ab06bfd72ed66
Liberbell <liberbell@gmail.com>
2019-06-11 15:30:23 +0900

    numbers6

--- numbers.go
+++ numbers.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var x float64
-	var y float64
+	// var x float64
+	// var y float64
 
-	x = 1
-	y = 2
+	x := 1.0
+	y := 2.0
 
 	fmt.Printf("x=%v, type of %T\n", x, x)
 	fmt.Printf("y=%v, type of %T\n", y, y)
