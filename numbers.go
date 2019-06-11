3735a20abfb369685bdceb0adbf9bb03680579d6
Liberbell <liberbell@gmail.com>
2019-06-11 15:31:16 +0900

    numbers7

--- numbers.go
+++ numbers.go
@@ -12,7 +12,7 @@ func main() {
 	fmt.Printf("x=%v, type of %T\n", x, x)
 	fmt.Printf("y=%v, type of %T\n", y, y)
 
-	var mean float64
-	mean = (x + y) / 2.0
+	// var mean float64
+	mean := (x + y) / 2.0
 	fmt.Printf("result: %v, type of %T\n", mean, mean)
 }
