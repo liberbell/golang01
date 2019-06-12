7f7755c4c73e197a8ff41849ff5514fedc58ec71
Liberbell <liberbell@gmail.com>
2019-06-13 08:09:56 +0900

    strings8

--- strings.go
+++ strings.go
@@ -10,5 +10,9 @@ func main() {
 
 	fmt.Printf("book[0] = %v (type %T)\n", book[0], book[0])
 
-	book[0] = 116
+	// book[0] = 116
+
+	fmt.Println(book[4:11])
+	fmt.Println(book[4:])
+	fmt.Println(book[:4])
 }
