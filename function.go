7b5c132b5cb0caa758af774bffaf69e3507e4679
Liberbell <liberbell@gmail.com>
2019-06-18 18:41:13 +0900

    function4

--- function.go
+++ function.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func add(a int, b int) int {
 	return a + b
 }
@@ -10,4 +12,5 @@ func divmod(a int, b int) (int, int) {
 
 func main() {
 	val := add(1, 2)
+	fmt.Println(val)
 }
