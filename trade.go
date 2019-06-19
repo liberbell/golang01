f4b75d2e87c1d84b8619d87b918abbeb2d00f2de
Liberbell <liberbell@gmail.com>
2019-06-20 08:32:51 +0900

    trade3

--- trade.go
+++ trade.go
@@ -1,8 +1,17 @@
 package main
 
+import "fmt"
+
 type Trade struct {
 	Symbol string
 	Volume int
 	Price  float64
 	Buy    bool
+}
+
+func main() {
+	t1 := Trade{"MSFT", 10, 99.98, true}
+	fmt.Println(t1)
+
+	fmt.Printf("%+v\n", t1)
 }
