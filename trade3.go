ebfbd2cf9db3110fc45391ce6cadd90df2f85ead
Liberbell <liberbell@gmail.com>
2019-06-22 12:16:38 +0900

    trade3-3

--- trade3.go
+++ trade3.go
@@ -6,4 +6,12 @@ func NewTrade(Symbol string, Volume int, Price float64, Buy bool) (*Trade, error
 	if Symbol == "" {
 		return nil, fmt.Errorf("Symbol can`t be Empty")
 	}
+
+  if Volume <= 0 {
+    return nil, fmt.Errorf("Volume must be >= 0 (was %d)", Volume)
+  }
+
+  if Price <= 0 {
+    return nil, 
+  }
 }
