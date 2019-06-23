f4f1b0d9fa80cdeb6747a1cf8ccc5e80423494af
Liberbell <liberbell@gmail.com>
2019-06-23 12:50:54 +0900

    square4

--- square.go
+++ square.go
@@ -4,8 +4,11 @@ func NewSquare(x int, y int, length int) (*Square, error) {
   if length <= 0 {
     return nil, fmt.Errorf("Length must be > 0")
   }
+
   s := &Square {
     Center := Point{x, y},
     Length := length,
   }
+  return s, nil
+  
 }
