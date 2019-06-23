b0d1556cf1db99a614449a9375195fff0789fc55
Liberbell <liberbell@gmail.com>
2019-06-23 12:47:25 +0900

    square2

---
+++ square.go
@@ -0,0 +1,11 @@
+package main
+
+func NewSquare(x int, y int, length int) (*Square, error) {
+  if length <= 0 {
+    return nil, fmt.Errorf("Length must be > 0")
+  }
+  s := &Square {
+    Center := Point{x, y}
+    Length := length,
+  }
+}
