8d2aef45d708c03ce22464201b954be028d3c2e9
Liberbell <liberbell@gmail.com>
2019-06-23 12:58:51 +0900

    square7

--- square.go
+++ square.go
@@ -1,33 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"log"
+)
+
 type Square struct {
-  Center Point
-  Length int
+	Center Point
+	Length int
 }
 
 func NewSquare(x int, y int, length int) (*Square, error) {
-  if length <= 0 {
-    return nil, fmt.Errorf("Length must be > 0")
-  }
+	if length <= 0 {
+		return nil, fmt.Errorf("Length must be > 0")
+	}
 
-  s := &Square {
-    Center := Point{x, y},
-    Length := length,
-  }
-  return s, nil
+	s := &Square{
+		Center: Point{x, y},
+		Length: length,
+	}
+	return s, nil
 }
 
 func (s *Square) Move(dx int, dy int) {
-  s.Center.Move(dx, dy)
+	s.Center.Move(dx, dy)
 }
 
 func main() {
-  s, err := NewSquare(1, 1, 10)
-  if err != nil {
-    log.Fatalf("ERROR: can`t create square.")
-  }
+	s, err := NewSquare(1, 1, 10)
+	if err != nil {
+		log.Fatalf("ERROR: can`t create square.")
+	}
 
-  s.Move(2, 3)
-  fmt.Printf("%+v\n", s)
-  fmt.Println(s.Area())
+	s.Move(2, 3)
+	fmt.Printf("%+v\n", s)
+	fmt.Println(s.Area())
 }
