e023452ce1af86a8067c581133336f4784811629
Liberbell <liberbell@gmail.com>
2019-06-26 07:36:43 +0900

    panic3

--- panic.go
+++ panic.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import "os"
 
 func main() {
-	vals := []int{1, 2, 3}
-	v := vals[10]
-	fmt.Println(v)
+	// vals := []int{1, 2, 3}
+	// v := vals[10]
+	// fmt.Println(v)
+	file, err := os.Open("no-such-file")
+	if err != nil {
+		panic(err)
+	}
 }
