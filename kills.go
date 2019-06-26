377385e02c2641100a6e5df81f2b874e4e9a48d2
Liberbell <liberbell@gmail.com>
2019-06-27 07:34:46 +0900

    kills1

--- kills.go
+++ kills.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"io/ioutil"
+	"log"
+	"os"
 
 	"github.com/pkg/errors"
 )
@@ -10,5 +12,9 @@ func killServer(pidFile string) error {
 	data, err := ioutil.ReadFile(pidFile)
 	if err != nil {
 		return errors.Wrap(err, "Can`t open pid file (is server runnig?)")
+	}
+
+	if err := os.Remove(pidFile); err != nil {
+		log.Printf("warning: can`t remove pid file - %s", err)
 	}
 }
