da8012be6c2fac417a0e6f7e87a56b8e40a43670
Liberbell <liberbell@gmail.com>
2019-06-30 12:36:51 +0900

    sites2-2

--- sites2.go
+++ sites2.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func returnType(url string, out chan string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		out <- fmt.Sprintf("%s -> error: %s", url, err)
+		return
+	}
+}
