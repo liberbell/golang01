03e5cf92c3ba1fee30921ddf36882dabc05c4cb9
Liberbell <liberbell@gmail.com>
2019-07-01 18:21:03 +0900

    md5-2

--- md5.go
+++ md5.go
@@ -1,1 +1,11 @@
 package main
+
+import "os"
+
+func parseSignatureFile(path string) (map[string]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+}
