2aaf4691f8326a47188bbf8c4ea8759fe1d4f03b
Liberbell <liberbell@gmail.com>
2019-07-01 18:24:02 +0900

    md5-3

--- md5.go
+++ md5.go
@@ -1,6 +1,11 @@
 package main
 
-import "os"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func parseSignatureFile(path string) (map[string]string, error) {
 	file, err := os.Open(path)
@@ -8,4 +13,13 @@ func parseSignatureFile(path string) (map[string]string, error) {
 		return nil, err
 	}
 	defer file.Close()
+
+	sigs := make(map[string]string)
+	scanner := bufio.NewScanner(file)
+	for lnum := 1; scanner.Scan(); lnum++ {
+		fileds := strings.Fields(scanner.Text())
+		if len(fields) != 2 {
+			return nil, fmt.Errorf("%s:%d bad line", path, lnum)
+		}
+	}
 }
