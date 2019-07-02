17d848b29fd3e4d8b90caf0312331d9bb6b1bcf3
Liberbell <liberbell@gmail.com>
2019-07-02 14:56:09 +0900

    cfg3

--- cfg.go
+++ cfg.go
@@ -1,8 +1,22 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 type Config struct {
 	Login struct {
 		User     string
 		Password string
 	}
+}
+
+func main() {
+	file, err := os.Open("config.toml")
+	if err != nil {
+		log.Fatalf("error: can`t open config file - %s\n", err)
+	}
+
+	defer file.Close()
 }
