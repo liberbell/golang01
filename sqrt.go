94a9c70b8e561f533c60d23d203ffc8549b68b9f
Liberbell <liberbell@gmail.com>
2019-07-03 07:46:54 +0900

    sqrt4

--- sqrt.go
+++ sqrt.go
@@ -15,4 +15,11 @@ func Sqrt(val float64) (float64, error) {
 	if val == 0.0 {
 		return 0.0, nil
 	}
+
+	guess, epsilon := 1.0, 0.00001
+	for i := 0; i < 10000; i++ {
+		if Abs(guess*guess-val) <= epsilon {
+			return guess, nil
+		}
+	}
 }
