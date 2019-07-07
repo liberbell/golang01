04fc279c8b4d5c3c6ab82220ec5a0d46015afd40
Liberbell <liberbell@gmail.com>
2019-07-07 15:02:50 +0900

    githubapi3

--- githubapi.go
+++ githubapi.go
@@ -1,6 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 type User struct {
 	Name        string `json:"name"`
 	PublicRepos int    `json:"public_repos"`
+}
+
+func userInfo(login string) (*User, error) {
+	url := fmt.Sprintf("https://api.github.com/users/%s", login)
+	resp, err := http.Get(url)
 }
