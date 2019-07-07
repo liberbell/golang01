93209e8d33d23fb2cc137707c30b04455536b5bb
Liberbell <liberbell@gmail.com>
2019-07-07 15:04:13 +0900

    githubapi4

--- githubapi.go
+++ githubapi.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -13,4 +14,14 @@ type User struct {
 func userInfo(login string) (*User, error) {
 	url := fmt.Sprintf("https://api.github.com/users/%s", login)
 	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	user := &User{}
+	dec := json.NewDecoder(resp.Body)
+	if err := dec.Decode(user); err != nil {
+		return nil, err
+	}
 }
