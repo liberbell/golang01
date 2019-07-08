00691ac9f0f329ffbb00b40b8e30b495b279df80
Liberbell <liberbell@gmail.com>
2019-07-08 09:56:57 +0900

    httpd3

--- httpd.go
+++ httpd.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello Gophers!")
+}
+
+func main() {
+	http.HandleFunc("/hello", helloHandler)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf(err)
+	}
 }
