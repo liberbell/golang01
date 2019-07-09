40620a04960f5196f4ed874cf92533e0b825c5a5
Liberbell <liberbell@gmail.com>
2019-07-09 22:04:17 +0900

    httpd3

--- httpd.go
+++ httpd.go
@@ -49,6 +49,10 @@ func mathHandler(w http.ResponseWriter, r *http.Request) {
 		resp.Error = fmt.Sprintf("unknown operation: %s", req.Op)
 	}
 	w.Handler().Set("Content-Type", "application/json")
+  if resp.Error != "" {
+    w.WriteHeader(http.StatusBadRequest)
+  }
+  }
 }
 
 func main() {
