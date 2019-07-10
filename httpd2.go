592b38db31631d3a79a41e00d8e43c93c8288f74
Liberbell <liberbell@gmail.com>
2019-07-10 15:31:52 +0900

    httpd2-15

--- httpd2.go
+++ httpd2.go
@@ -15,7 +15,7 @@ var (
 
 type Entry struct {
 	Key   string      `json:"key"`
-	Value interface{} `json:value`
+	Value interface{} `json:"value"`
 }
 
 func sendResponse(entry *Entry, w http.ResponseWriter) {
@@ -60,61 +60,9 @@ func kvGetHandler(w http.ResponseWriter, r *http.Request) {
 	sendResponse(entry, w)
 }
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello Gophers!")
-}
-
-type MathRequest struct {
-	Op    string  `json:"op"`
-	Left  float64 `json:"left"`
-	Right float64 `json:"right"`
-}
-
-type MathResponse struct {
-	Error  string  `json:"error"`
-	Result float64 `json:result`
-}
-
-func mathHandler(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	dec := json.NewDecoder(r.Body)
-	req := &MathRequest{}
-
-	if err := dec.Decode(req); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	resp := &MathResponse{}
-	switch req.Op {
-	case "+":
-		resp.Result = req.Left + req.Right
-	case "-":
-		resp.Result = req.Left - req.Right
-	case "*":
-		resp.Result = req.Left * req.Right
-	case "/":
-		if req.Right == 0.0 {
-			resp.Error = "division by 0"
-		} else {
-			resp.Result = req.Left / req.Right
-		}
-	default:
-		resp.Error = fmt.Sprintf("unknown operation: %s", req.Op)
-	}
-	w.Header().Set("Content-Type", "application/json")
-	if resp.Error != "" {
-		w.WriteHeader(http.StatusBadRequest)
-	}
-
-	enc := json.NewEncoder(w)
-	if err := enc.Encode(resp); err != nil {
-		log.Printf("can`t encode %v - %s", resp, err)
-	}
-}
-
 func main() {
-	http.HandleFunc("/hello", helloHandler)
-	http.HandleFunc("/math", mathHandler)
+	// http.HandleFunc("/hello", helloHandler)
+	// http.HandleFunc("/math", mathHandler)
 	http.HnadleFunc("/db", kvPostHandler)
 	http.HandleFunc("/db/", kvGetHandler)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
