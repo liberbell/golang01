54fd9535c670d5bc562780deb54468fb1a6eaf32
Liberbell <liberbell@gmail.com>
2019-07-10 15:32:43 +0900

    httpd2-16

--- httpd2.go
+++ httpd2.go
@@ -63,7 +63,7 @@ func kvGetHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	// http.HandleFunc("/hello", helloHandler)
 	// http.HandleFunc("/math", mathHandler)
-	http.HnadleFunc("/db", kvPostHandler)
+	http.HandleFunc("/db", kvPostHandler)
 	http.HandleFunc("/db/", kvGetHandler)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
